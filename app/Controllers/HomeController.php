44503f079103ca174f1b92d94190625d2eff49d8
BitAuthority <thebitauthority@gmail.com>
2018-10-29 12:23:17 +0100

    Fixed errors

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,11 +1,10 @@
 <?php
 namespace App\Controllers;
 
-use App\Requests\TestRequest;
 use EvolutionMedia\RevolPHP\Request;
 
 class HomeController extends \EvolutionMedia\RevolPHP\Controllers\Controller {
-    public function test(TestRequest $req)
+    public function test(Request $req)
     {
         echo 'Waddup';
     }
